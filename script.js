4b59921631e2665425eb2a1cc19976bbc75a7b95
michael <112956102+polikujydo@users.noreply.github.com>
2022-11-19 17:33:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
- const numberOfFilms = +prompt ("How many movies have u already watched?", ' ');
- const personalMovieDB = {
-     "count" : numberOfFilms,
-     "movies" : {},
-     "actors" : {},
-     "genres" : [],
-"privat" : false
+const numberOfFilms = +prompt ("How many movies have u already watched?", ' ');
+const personalMovieDB = {
+    "count" : numberOfFilms,
+    "movies" : {},
+    "actors" : {},
+    "genres" : [],
+    "privat" : false
 };
 
-const a = prompt("Your last viewed movie?",' '),
-  b = prompt("What is ur grade?", ' '),
-  c = prompt ("How many movies have u already watched?", ' ');
-  d = prompt("What is ur grade?", ' ');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    for (let i = 0; i < 2; i++){
+        const a = prompt("Your last viewed movie?",' '),
+              b = prompt("What is ur grade?", ' ');
+
+        if (a != null && b != null && a != " " && b != " " && a.length < 50){
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
+    }
+
+    if (personalMovieDB.count < 10){
+        console.log('less');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ){
+        console.log('Normal');
+    } else {
+        console.log("MovieHunter!");
+    }
+
+
 console.log(personalMovieDB);
