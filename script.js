32c165b8aad609d9b7741fb18d60cc86ce17ad6e
michael <112956102+polikujydo@users.noreply.github.com>
2022-11-19 15:39:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// const numberOfFilms = +prompt ("How many movies have u already watched?", ' ');
+// const personalMovieDB = {
+//     "count" : numberOfFilms,
+//     "movies" : {},
+//     "actors" : {},
+//     "genres" : [],
+//     "privat" : false
+// };
+
+// const a = prompt("Your last viewed movie?",' '),
+//     b = prompt("What is ur grade?", ' '),
+//     c = prompt ("How many movies have u already watched?", ' ');
+//     d = prompt("What is ur grade?", ' ');
+
+//     personalMovieDB.movies[a] = b;
+//     personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
